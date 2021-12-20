28ffb96eeece911e2b44cc5be797fe1c0658dc0f
Laurynas <laurynas.balsys91@gmail.com>
2021-12-20 20:39:47 +0200

    Report Item

--- Airlines/Library/ReportGenerator.cs
+++ Airlines/Library/ReportGenerator.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class ReportGenerator
+    public class ReportGenerator
     {
+        private AircraftRepository _aircraftRepository;
+        private AircraftModelRepository _aircraftModelRepository;
+        private CompanyRepository _companyRepository;
+        private CountryRepository _countryRepository;
+
+        public ReportGenerator(AircraftRepository aircraftRepository, AircraftModelRepository aircraftModelRepository, CompanyRepository companyRepository, CountryRepository countryRepository)
+        {
+            _aircraftRepository = aircraftRepository;
+            _aircraftModelRepository = aircraftModelRepository;
+            _companyRepository = companyRepository;
+            _countryRepository = countryRepository;
+        }
+        public List<ReportItem> GenerateReportAircraftInEurope()
+        {
+            List<Aircraft> aircrafts = _aircraftRepository.Retrieve();
+            foreach (var aircraft in aircrafts)
+            {
+
+            }
+            
+            return;
+        }
+
     }
 }
